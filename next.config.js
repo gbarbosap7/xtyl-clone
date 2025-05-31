32eb95553093a1936a0a7506df9a2a5480ed2189
gbarbosap7 <gustavobarbosapix@gmail.com>
2025-05-31 18:30:32 -0300

    Corrigindo configuração do Tailwind CSS

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
   images: {
     domains: ['xtyl.digital'],
   },
